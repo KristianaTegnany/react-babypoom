0201c124f0efcef516fc6a270ce09aab800463a0
Guillaume Tournier <guillaume.tournier@originate.com>
2018-08-17 18:14:17 -0700

    no trademark in hd mode

--- album-photo-app/components/page/index.js
+++ album-photo-app/components/page/index.js
@@ -1,15 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 import cx from '../../../lib/cx'
 
 import styles from './styles.scss'
 
-let Page = ({ reverse, children, className, ...props }) => {
-  return (
-    <div styleName={cx({ page: true, reverse })} className={className} {...props}>
-      {children}
-    </div>
-  )
+@connect(mapStateToProps)
+class Page extends Component {
+  render() {
+    let { reverse, children, className, media, ...props } = this.props
+    return (
+      <div styleName={cx({ page: true, reverse, trademark: 'print' !== media })} className={className} {...props}>
+        {children}
+      </div>
+    )
+  }
 }
 
 export default Page
+
+function mapStateToProps(state) {
+  const {
+    app: { media },
+  } = state
+  return { media }
+}
