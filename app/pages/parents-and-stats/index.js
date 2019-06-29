aedf032e00d5e62d0bc44e91381a390e165c144b
Guillaume Tournier <guillaume.tournier@originate.com>
2019-06-29 00:46:47 -0700

    - default photos for parents - eslint 6 & prettier are nemesis - issues with empty parent reaction / name

--- app/pages/parents-and-stats/index.js
+++ app/pages/parents-and-stats/index.js
@@ -5,67 +5,86 @@ import Title from '../../components/title'
 import PresentationPanel from '../../components/presentation-panel'
 import ContentPanel from '../../components/content-panel'
 import getPhoto from '../../../lib/get-photo'
-import defaultPhoto from '../../images/default.jpeg'
 import t from '../../i18n/i18n'
-import { defineMessages, FormattedDate } from 'react-intl'
+import { defineMessages } from 'react-intl'
 import range from '../../../lib/range'
-import styles from './styles.scss'
+import parent1Default from '../../images/parent-1.svg'
+import parent2Default from '../../images/parent-2.svg'
+import './styles.scss'
+
+const defaulPhotos = {
+  1: parent1Default,
+  2: parent2Default,
+}
 
 class ParentsAndStats extends Component {
   render() {
     let { bpoom, params } = this.props
 
-    return [
-      <Page key="parents">
-        <PresentationPanel styleName="parents-presentation-panel">
-          <Title label={t(MSG.parent_reactions)} />
-        </PresentationPanel>
-        <ContentPanel centered>
-          {range(1, 2).map(index => (
-            <div key={index} styleName={`parents-reaction parent-${index}`}>
-              <div>
-                <div
-                  style={{
-                    backgroundImage: `url(${getPhoto(bpoom[`parent_${index}_photo_urls`], params.hd) || defaultPhoto})`,
-                  }}
-                  styleName="image-container"
-                />
-                {bpoom[`parent_${index}_reaction`] && (
-                  <div styleName="quote-container">
-                    <p styleName="quote">
-                      {bpoom[`parent_${index}_reaction`]}
-                      <br />-<br />
-                      {bpoom[`parent_${index}_name`]}
-                    </p>
+    return (
+      <React.Fragment>
+        {(bpoom.parent_1_reaction || bpoom.parent_2_reaction) && (
+          <Page>
+            <PresentationPanel styleName="parents-presentation-panel">
+              <Title label={t(MSG.parent_reactions)} />
+            </PresentationPanel>
+            <ContentPanel centered>
+              {range(1, 2).map(index => {
+                const reaction = bpoom[`parent_${index}_reaction`]
+                const name = bpoom[`parent_${index}_name`]
+                return (
+                  <div key={index} styleName={`parents-reaction parent-${index}`}>
+                    <div>
+                      <div
+                        style={{
+                          backgroundImage: `url(${getPhoto(bpoom[`parent_${index}_photo_urls`], params.hd) ||
+                            defaulPhotos[index]})`,
+                        }}
+                        styleName="image-container"
+                      />
+                      {reaction && (
+                        <div styleName="quote-container">
+                          <p styleName="quote">
+                            {reaction}
+                            {name && (
+                              <React.Fragment>
+                                <br />-<br />
+                                {name}
+                              </React.Fragment>
+                            )}
+                          </p>
+                        </div>
+                      )}
+                    </div>
                   </div>
-                )}
-              </div>
-            </div>
-          ))}
-        </ContentPanel>
-      </Page>,
-      <Page key="stats">
-        <PresentationPanel styleName="stat-presentation-panel">
-          <Title label={t(MSG.babypoom_results)} />
-        </PresentationPanel>
-        <ContentPanel background centered>
-          <ul styleName="list">
-            {[
-              ['tries', bpoom.game_tries_avg, t(MSG.stat_tries)],
-              ['time', t({ ...MSG.time_in_sec, values: { seconds: bpoom.game_time_avg } }), t(MSG.stat_time)],
-              ['visits', bpoom.visit_count, t(MSG.stat_visits)],
-              ['messages', bpoom.message_count, t(MSG.stat_messages)],
-            ].map(([type, metric, text]) => (
-              <li key={type}>
-                <i styleName={`${type}-icon`} />
-                <span styleName="result">{metric}</span>
-                <span styleName="desc">{text}</span>
-              </li>
-            ))}
-          </ul>
-        </ContentPanel>
-      </Page>,
-    ]
+                )
+              })}
+            </ContentPanel>
+          </Page>
+        )}
+        <Page>
+          <PresentationPanel styleName="stat-presentation-panel">
+            <Title label={t(MSG.babypoom_results)} />
+          </PresentationPanel>
+          <ContentPanel background centered>
+            <ul styleName="list">
+              {[
+                ['tries', bpoom.game_tries_avg, t(MSG.stat_tries)],
+                ['time', t({ ...MSG.time_in_sec, values: { seconds: bpoom.game_time_avg } }), t(MSG.stat_time)],
+                ['visits', bpoom.visit_count, t(MSG.stat_visits)],
+                ['messages', bpoom.message_count, t(MSG.stat_messages)],
+              ].map(([type, metric, text]) => (
+                <li key={type}>
+                  <i styleName={`${type}-icon`} />
+                  <span styleName="result">{metric}</span>
+                  <span styleName="desc">{text}</span>
+                </li>
+              ))}
+            </ul>
+          </ContentPanel>
+        </Page>
+      </React.Fragment>
+    )
   }
 }
 
