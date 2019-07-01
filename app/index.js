525f08974e08dd5ec9a8273589a3705547c67934
Guillaume Tournier <guillaume.tournier@originate.com>
2019-07-01 00:49:43 -0700

    - improve api - http -> https auto redirect

--- app/index.js
+++ app/index.js
@@ -8,11 +8,15 @@ import { renderRoutes } from 'react-router-config'
 import routes from './routes'
 import configureStore from './store/configureStore'
 import HotIntlProvider from './i18n/hot-intl-provider/HotIntlProvider'
-import { updateLocale } from './i18n/hot-intl-provider/HotIntlProviderActions'
 import { updateParams } from './views/app/Actions'
 import 'isomorphic-fetch'
 import config from '../config'
+import isLocalhost from '../lib/is-localhost'
 import { queryParams } from '../lib/url-params'
+
+if (!isLocalhost() && location.protocol === 'http:') {
+  window.location.href = location.href.replace('http', 'https')
+}
 
 // Store
 const initialState = window[config.requestCacheVar]
