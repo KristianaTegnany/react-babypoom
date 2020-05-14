e4ffc42f96d6cfeaeefee4983ab16e192fd6df88
jidylle <jerome.idylle@gmail.com>
2020-05-14 17:54:01 -0400

    fix ux ui issues on Visitor book, Pot and game

--- app/views/guest-book-form/index.js
+++ app/views/guest-book-form/index.js
@@ -19,6 +19,7 @@ import { InputField, CheckField } from '../../../lib/redux-form-input'
 import { extractParams } from '../../../lib/params'
 import Ahoy from '../../../lib/ahoy-custom'
 import imgPath from '../../../lib/img-path'
+import { Prompt } from 'react-router'
 import api from '../../api'
 import './styles.scss'
 
@@ -142,6 +143,14 @@ let GuestBookForm = ({ bpoom, btnColor, flash, api, saveMsg, onSave, onCancel })
               </div>
             </div>
             <div styleName="actions">
+              <Prompt
+                when={!isSubmitting}
+                message={(location) => {
+                  return location.pathname.endsWith('/pot') || location.pathname.endsWith('/souvenir')
+                    ? `Es-tu sûr de vouloir continuer sans valider ton message :/ ?`
+                    : true
+                }}
+              />
               <Button color="neutral-app" onClick={() => onCancel && onCancel()}>
                 {t(FORM_MSG.form_cancel)}
               </Button>
