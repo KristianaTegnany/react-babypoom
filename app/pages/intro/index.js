c705b2cf4ab696f54dc4d56916a9c2759cef3c09
jerome <jerome@babypoom.com>
2022-01-10 21:08:51 -0400

    fix intro

--- app/pages/intro/index.js
+++ app/pages/intro/index.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 import Page from '../../components/page'
 import Title from '../../components/title'
 import PresentationPanel from '../../components/presentation-panel'
@@ -8,31 +9,48 @@ import t from '../../i18n/i18n'
 import { defineMessages } from 'react-intl'
 import './styles.theme-BP_ALBUM_THEME.scss'
 
-let Intro = () => [
-  <Page key="intro-blank" />, // Blank page
-  <Page key="intro" styleName="page">
-    <PresentationPanel styleName="presentation-panel">
-      <Title general label={t(MSG.my_photo_album)} styleName="titleColor"/>
-    </PresentationPanel>
-    <ContentPanel styleName="content-panel">
-      <BorderBgBox styleName="border-box">
-        <div styleName="content">
-          <p styleName="description">{t(MSG.my_photo_album_desc)}</p>
-          <ol styleName="list">
-            <li>{t(MSG.arrival)}</li>
-            <li>{t(MSG.trip)}</li>
-            <li>{t(MSG.souvenir)}</li>
-            <li>{t(MSG.guestbook)}</li>
-            <li>{t(MSG.reaction)}</li>
-            <li>{t(MSG.results)}</li>
-          </ol>
-        </div>
-      </BorderBgBox>
-    </ContentPanel>
-  </Page>,
-]
+class Intro extends Component {
+  render() {
+    let { bpoom } = this.props
+    let { bpoom: { trip_events = [] } = {} } = this.props
 
-export default Intro
+    return (
+      <>
+        <Page key="intro-blank" />
+        <Page key="intro" styleName="page">
+          <PresentationPanel styleName="presentation-panel">
+            <Title general label={t(MSG.my_photo_album)} styleName="titleColor" />
+          </PresentationPanel>
+          <ContentPanel styleName="content-panel">
+            <BorderBgBox styleName="border-box">
+              <div styleName="content">
+                <p styleName="description">{t(MSG.my_photo_album_desc)}</p>
+                <ol styleName="list">
+                  <li>{t(MSG.arrival)}</li>
+                  {trip_events.length && <li>{t(MSG.trip)}</li>}
+                  <li>{t(MSG.souvenir)}</li>
+                  {bpoom.firstname_infos && <li>{t(MSG.firstname)}</li>}
+                  <li>{t(MSG.guestbook)}</li>
+                  <li>{t(MSG.reaction)}</li>
+                  <li>{t(MSG.results)}</li>
+                </ol>
+              </div>
+            </BorderBgBox>
+          </ContentPanel>
+        </Page>
+      </>
+    )
+  }
+}
+
+export default connect(mapStateToProps)(Intro)
+
+function mapStateToProps(state) {
+  const {
+    app: { bpoom, params },
+  } = state
+  return { bpoom, params }
+}
 
 const MSG = defineMessages({
   my_photo_album: {
@@ -57,6 +75,10 @@ marqué mon arrivée dans notre monde.`,
   souvenir: {
     id: 'intro.souvenir',
     defaultMessage: `Mon faire-part`,
+  },
+  firstname: {
+    id: 'intro.fistname',
+    defaultMessage: `Mon prénom`,
   },
   guestbook: {
     id: 'intro.guestbook',
